1ea653afda3af9b77cc81260e9b310cd9b5f5fe6
mayo7e <mayowa.wh@gmail.com>
2023-09-23 17:27:39 +0100

    Set up component, asset ans styles for header and home

---
+++ app/index.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { View, Button, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { 
+    SIZES, COLORS, icons, images
+ } from "../constants";
+
+import { 
+     Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome
+ } from "../components"
+
+
+const Home = () => {
+const route = useRouter()
+
+    return ( 
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }} >
+    
+                <Stack.Screen
+                    options={{
+                        headerStyle: { backgroundColor: COLORS.primary },
+                        headerShadowVisible: false,
+                        headerLeft: ()=>(
+                            <ScreenHeaderBtn iconUrl = {icons.menu} dimension ="60%" />
+                
+                        ),
+                        headerRight: ()=>(
+                            <ScreenHeaderBtn iconUrl = {images.profile} dimension ="100%%" />
+                        ),
+                        headerTitle: "",
+                    }}
+                />
+
+                <ScrollView showsVerticalScrollIndicator={false} >
+                    <View 
+                        style={{
+                            flex: 1,
+                            padding: SIZES.medium
+                        }}
+                    >
+                            <Welcome />
+                            <Popularjobs />
+                            <Nearbyjobs />
+                            <Nearbyjobs />
+                    </View>
+                </ScrollView>
+               
+        </SafeAreaView>
+            
+    
+     );
+}
+ 
+export default Home;
