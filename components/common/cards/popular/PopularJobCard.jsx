7e3b9691872ad587ade40ccd647c8a0102bb9d5c
mayo7e <mayowa.wh@gmail.com>
2023-09-27 02:22:47 +0100

    Update popularJobCard - location

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -39,9 +39,12 @@ const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
 
         
         <View stylw={styles.infoContainer} >
-          <Text style={styles.jobName(selectedJob, item)} >
+
+          <Text style={styles.jobName(selectedJob, item)}  numberOfLines={1} >
             {item.job_title}
           </Text>
+
+          <Text style={styles.location}> {item.job_country} </Text>
 
         </View>
 
