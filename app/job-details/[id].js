66eebd19a0f77f64918411ace13c714228573f17
mayo7e <mayowa.wh@gmail.com>
2023-10-06 07:41:15 +0100

    Update footer section of job-details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -112,6 +112,10 @@ const JobDetails = () => {
                         )
                     }
                     </ScrollView>
+
+                    <JobFooter 
+                        url= {data[0]?.job_google_link ?? "https://careers.google.com/jobs/results" }
+                    />
                 </>
 
              
